71f4402fb544a03fc51f1fbf8dccf45b6d1f8da2
msxshankar <sc21ms@leeds.ac.uk>
2022-10-12 18:47:57 +0100

    Initial commit

---
+++ graphcalc.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "graphcalc.h"
+
+int main () {
+    Calc f;
+    std::cout << f.k << std::endl;
+
+	return 0;
+}
---
+++ graphcalc.h
@@ -0,0 +1,7 @@
+// Class definitions
+
+class Calc {
+public:
+    int k;
+    Calc() : k(10) {}
+};
